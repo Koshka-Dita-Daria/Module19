829536fabfa93e0d18738772eadb98634fe39f46
Koshka-Dita-Daria <90615238+Koshka-Dita-Daria@users.noreply.github.com>
2024-11-26 18:42:44 +0300

    Add files via upload

--- pr/task1/models.py
+++ pr/task1/models.py
@@ -13,8 +13,8 @@ class Buyer(models.Model):
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=100)
-    cost = models.DecimalField(max_digits=1000000, decimal_places=2)
-    size = models.DecimalField(max_digits=1000000, decimal_places=2)
+    cost = models.DecimalField(max_digits=9, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=4)
     description = models.CharField(max_length=10000000)
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(to=Buyer, related_name='games')
